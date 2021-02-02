d0dd9bd473d2f77c1d4aa7aa6cae71e8e9301a1e
Kate McAleenan <kate.mca@hotmail.co.uk>
2021-02-02 19:08:51 +0000

    search functionality & pop up modals

--- main.js
+++ main.js
@@ -21,91 +21,132 @@ $(document).ready(function(){
 });
 });
 
-function searchData() {
-    var e = $("#search").val();
-    "" !== e
-        ? $.ajax({
-              type: "GET",
-              dataType: "jsonp",
-              cache: !1,
-              url: "https://content.guardianapis.com/search?q=" + e + "&order-by=relevance&api-key=f2501b62-5dde-4bda-aaf2-3f824948fec8&show-fields=thumbnail,body,headline",
-              success: function (t) {
-                  if ((console.log("Your search has been made"), $(".searchContainer").show(), $("html, body").animate({ scrollTop: $(".searchContainer").offset().top - 10 }, 2e3), 0 == t.response.total))
-                      $(".loadSearchResults").html("No results found against this search.");
-                  else {
-                      if (!window.localStorage.getItem("search" + e)) {
-                          var n = JSON.stringify(t.response.results);
-                          window.localStorage.setItem("search" + e, n);
-                      }
-                      $(".loadSearchResults").html("");
-                      for (var i = 0; i < 9; i++) {
-                          var o = "";
-                          i % 3 == 0 && (o = "first");
-                        //   var s = diff_minutes(new Date(t.response.results[i].webPublicationDate));
-                          $(".loadSearchResults").append(
-                              '<li class="one_third ' +
-                                  o +
-                                  '">\n                                                <figure>\n                                                    <div class="banner-title">' +
-                                  t.response.results[i].sectionName +
-                                  '</div>\n                                                    <img src="' +
-                                  t.response.results[i].fields.thumbnail +
-                                  '" alt="">\n                                                    <figcaption>\n                                                        <p>' +
-                                  t.response.results[i].fields.headline +
-                                  '</p>\n                                                        <p class="time-difference"><small><em>' +
-                                //   s +
-                                  '</em></small></p>\n                                                        <footer><label class="btn pointer"  for="modal_search_' +
-                                  i +
-                                  '">Read More</label></footer>\n                                                    </figcaption>\n                                                </figure>\n                                            </li>\n                                            <input class="checker" type="checkbox" id="modal_search_' +
-                                  i +
-                                  '">\n                                            <div class="modal">\n                                                <div class="modal-body">\n                                                    <label class="btn_close" for="modal_search_' +
-                                  i +
-                                  '">Close</label>\n                                                    <h6 class="heading">' +
-                                  t.response.results[i].fields.headline +
-                                  '</h6>\n                                                    <img src="' +
-                                  t.response.results[i].fields.thumbnail +
-                                  '" alt="">\n                                                    <div class="modal-content">' +
-                                  t.response.results[i].fields.body +
-                                  "</div>\n                                                </div>\n                                            </div>"
-                          );
-                      }
-                  }
-              },
-          }).fail(function (t, n, i) {
-              if (($(".loadSearchResults").html(""), $(".searchContainer").show(), $("html, body").animate({ scrollTop: $(".searchContainer").offset().top - 10 }, 2e3), window.localStorage.getItem("search" + e))) {
-                  (resultss = window.localStorage.getItem("search" + e)), (results = JSON.parse(resultss));
-                  for (var o = 0; o < results.length; o++) {
-                      var s = "";
-                      o % 3 == 0 && (s = "first");
-                      var r = results[o];
-                        //   a = diff_minutes(new Date(r.webPublicationDate));
-                      $(".loadSearchResults").append(
-                          '<li class="one_third ' +
-                              s +
-                              '">\n                            <figure>\n                                <div class="banner-title">' +
-                              r.sectionName +
-                              '</div>\n                                <img src="' +
-                              r.fields.thumbnail +
-                              '" alt="">\n                                <figcaption>\n                                    <p>' +
-                              r.fields.headline +
-                              '</p>\n                                    <p class="time-difference"><small><em>' +
-                            //   a +
-                              '</em></small></p>\n                                    <footer><label class="btn pointer"  for="modal_search_' +
-                              o +
-                              '">Read More</label></footer>\n                                </figcaption>\n                            </figure>\n                        </li>\n                        <input class="checker" type="checkbox" id="modal_search_' +
-                              o +
-                              '">\n                        <div class="modal">\n                            <div class="modal-body">\n                                <label class="btn_close" for="modal_search_' +
-                              o +
-                              '">Close</label>\n                                <h6 class="heading">' +
-                              r.fields.headline +
-                              '</h6>\n                                <img src="' +
-                              r.fields.thumbnail +
-                              '" alt="">\n                                <div class="modal-content">' +
-                              r.fields.body +
-                              "</div>\n                            </div>\n                        </div>"
-                      );
-                  }
-                  $(".searchContainer").show();
-              } else console.log("no results"), $(".loadSearchResults").html("No data found against this search.");
-          })
-        : ($(".searchContainer").hide(), $(".loadSearchResults").html(""));
+//------------------------------------------------------ SEARCH ------------------------------------------//
+//**
+// 1 - Check if search is not empty then input the search value into the api url and order by relevance.
+// 2 - If api call is successful console log successful call.
+// 3 - Display search container and use animation to scroll to the section.
+// 4 - If results are 0 then return no results found.
+// 5 - using first class from css file to output only 3 items per line
+// 6 - If i==3 articles (we already added 3 articles) the variable first user the value "first" for another line 
+// 7 - If call result is within local storage then stringify results.
+// 8 - Add results to local storage using the search input & the value searched.
+// 9 - Render html using for loop to return first 9 results.
+//**
+function searchNews() {
+    var searchInput = $("#search").val();
+    if (searchInput !== "") {
+        $.ajax({
+            type: "GET",
+            dataType: "jsonp",
+            cache: false,
+            url: "https://content.guardianapis.com/search?q=" + searchInput + "&order-by=relevance&api-key=f2501b62-5dde-4bda-aaf2-3f824948fec8&show-fields=thumbnail,body,headline",
+            success: function (data) {
+                console.log("Your search has been made");
+                $(".searchContainer").show();
+                $('html, body').animate({
+                    scrollTop: $(".searchContainer").offset().top - 10
+                }, 2000);
+                if (data.response.total == 0) {
+                    $(".loadSearchResults").html('No results found against this search.');
+                } else {
+                    if (!window.localStorage.getItem("search" + searchInput)) {
+                        var dataResponse = JSON.stringify(data.response.results);
+                        window.localStorage.setItem("search" + searchInput, dataResponse);
+                    }
+                    $(".loadSearchResults").html("");
+                    for (var i = 0; i <= 8; i++) {
+                        var first = "";
+                        if (i % 3 == 0) {
+                            first = "first";
+                        }
+                        $(".loadSearchResults").append(
+                            `<li class="one_third ` + first + `">
+                                <figure>
+                                    <div class="banner-title">` + data.response.results[i].sectionName + `</div>
+                                    <img src="` + data.response.results[i].fields.thumbnail + `" alt="">
+                                    <figcaption>
+                                        <p>` + data.response.results[i].fields.headline + `</p>
+                                        <footer><label class="btn pointer"  for="modal_search_` + i + `">Read More</label></footer>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                            <input class="checker" type="checkbox" id="modal_search_` + i + `">
+                            <div class="modal">
+                                <div class="modal-body">
+                                    <label class="btn_close" for="modal_search_` + i + `">Close</label>
+                                    <h6 class="heading">` + data.response.results[i].fields.headline + `</h6>
+                                    <img src="` + data.response.results[i].fields.thumbnail + `" alt="">
+                                    <div class="modal-content">` + data.response.results[i].fields.body + `</div>
+                                </div>
+                            </div>`
+                            );
+                        }
+                    }
+                },
+            })
+
+            //------------------------------------------------------ OFFLINE ------------------------------------------//
+            //**
+            // 1 - If the API call fails then check if the search input exists by searching local storage.
+            // 2 - Parse the results of the search value from local storage into a JSON file and the render HTML.
+            //**
+        
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                if ($(".loadSearchResults").html(""),
+                $(".searchContainer").show(),
+                $("html, body").animate({
+                    scrollTop: $(".searchContainer").offset().top - 10
+                }, 2000),
+                window.localStorage.getItem("search" + searchInput)
+                ) {
+                    searchResults = window.localStorage.getItem("search"+ searchInput);
+                    results = JSON.parse(searchResults);
+                    for (var o = 0; o < results.length; o++) {
+                        var first = "";
+                        if (o % 3 == 0) {
+                            first = "first";
+                        }
+                        var response = results[o];
+                        $(".loadSearchResults").append(
+                            `<li class="one_third ` + first + `">
+                                <figure>
+                                    <div class="banner-title">` + response.sectionName + `</div>
+                                    <img src="` + response.fields.thumbnail + `" alt="">
+                                    <figcaption>
+                                        <p>` + response.fields.headline + `</p>
+                                        <footer><label class="btn pointer"  for="modal_search_` + o + `">Read More</label></footer>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                            <input class="checker" type="checkbox" id="modal_search_` + o + `">
+                            <div class="modal">
+                                <div class="modal-body">
+                                    <label class="btn_close" for="modal_search_` + o + `">Close</label>
+                                    <h6 class="heading">` + response.fields.headline + `</h6>
+                                    <img src="` + response.fields.thumbnail + `" alt="">
+                                    <div class="modal-content">` + response.fields.body + `</div>
+                                </div>
+                            </div>`
+                            );
+                        }
+                        $(".searchContainer").show();
+                    } else {
+                        console.log("no results"), $(".loadSearchResults").html("No data found against this search.");
+                    }
+                })
+            } else {
+                // hide the search section until a search is made
+                ($(".searchContainer").hide(),
+                $(".loadSearchResults").html(""))
+             } 
 }
+
+//------------------------------------------------------ CLOSE SEARCH -------------------------------------------------//
+//**
+// Using jQuery animation to hide search results section
+//**
+
+$( "#closeResults" ).click(function() {
+    $( ".searchContainer" ).slideUp(2000);
+ });
