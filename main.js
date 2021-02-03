2b8c8273cdacc4c99a1b747527949973dd783a05
Kate McAleenan <kate.mca@hotmail.co.uk>
2021-02-03 15:52:19 +0000

    Revert "search tidy up using bootstrap"
    This reverts commit 55d0904a8c9057433664848495f6f6e602e57415.

--- main.js
+++ main.js
@@ -61,21 +61,19 @@ function searchNews() {
                             first = "first";
                         }
                         $(".loadSearchResults").append(
-                            `
-                            <div class="col mb-4">
-                                <div class="card h-100">
-                                    <h5 class="card-header">` + data.response.results[i].sectionName +`</h5>
-                                    <div class="card-body">
-                                        <h5 class="card-title">`+ data.response.results[i].fields.headline +  `</h5>
-                                        <img src="` + data.response.results[i].fields.thumbnail + `" class="card-img-top" alt="">
-                                        <a href="#" class="btn btn-primary"><label for="modal_search_` + i + `">Read Full Story</label></a>
-                                    </div>
-                                </div>
-                            </div>
-                            
-    
+                            `<li class="one_third ` + first + `">
+                                <figure>
+                                    <div class="figure-section">` + data.response.results[i].sectionName + `</div>
+                                    <figcaption>
+                                        <p>` + data.response.results[i].fields.headline + `</p>
+                                        <footer><label class="btn btn-primary"  for="modal_search_` + i + `">Read Full Story</label>
+                                        </footer>
+                                    </figcaption>
+                                    <img src="` + data.response.results[i].fields.thumbnail + `" style="max-width:325px; border-radius:4px;" alt="">
+                                </figure>
+                            </li>
                             <input class="checker" type="checkbox" id="modal_search_` + i + `">
-                            <div class="modal" style="max-width: 100vw; padding: 40px;">
+                            <div class="modal">
                                 <div class="modal-body">
                                     <div class="modal-header">
                                         <h5 class="modal-title">` + data.response.results[i].fields.headline + `</h5>
