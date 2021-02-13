201e5dd6770fe7c77cf6e22e7c98f13f7f01b911
Kate McAleenan <kate.mca@hotmail.co.uk>
2021-02-13 14:06:28 +0000

    trending and styles

--- main.js
+++ main.js
@@ -1,14 +1,145 @@
 $(document).ready(function(){    
     getSections();
+    getTrendingNews();
 
     $(function() {
-        $(".variable").slick({
+        $(".slides").slick({
             dots: true,
             autoplay: true,
             autoplaySpeed: 3000
         });
 
     })
+
+//------------------------------------------------------- TRENDING SECTION ONLINE & OFFLINE FUNCTION  ------------------------------------------
+// ONLINE FUNCTION
+    //**
+    //Step 1 - Searching for the term Trending and then ordering the results order of Newest
+    //Step 2 - Creating an array for trending news and storying them in the local storage using headline as the key
+    //Step 3 - Initiating the html of the news section which here is uk
+    //Step 4 - Creating a loop for the API call to do the function 9 times
+    //Step 5 - Duplication check - check if key doesn't exists in localStorage, if not then store it with headline as the key
+    //Step 5 - Seperating the keys to array of world breaking the string by headline by dimeter "%%"
+    //Step 6 - Getting the date of the publication of the artical and putting it into the date function created to show how old the article is (diff_minutes)
+    //Step 7 - add html to the "loadUKStories" block resulting in the headline, date and thumbnial being output in cards
+    //Step 8 - the modal itself then starts at (<div class = "modal_body">....</div>)
+    //Step 9 - a checkbox is added to check if the article is clicked to view more so it shows the correpondant modal
+    //Step 10 - To have a correspondance between the modal full view and the article, I use the parameter "i" which is used to create the "id" of the checkbox 
+    //Step 11 - The footer is used to open the modal and the btn_close is used to close the modal full view when finished reading
+    //**    
+    function getTrendingNews() {
+        $.ajax({
+            type: "GET",
+            dataType: "jsonp",
+            cache: false,
+            url: "https://content.guardianapis.com/search?q=Trending&order-by=newest&api-key=f2501b62-5dde-4bda-aaf2-3f824948fec&show-fields=thumbnail,body,headline",
+            success: function (data) {
+                // create an array for Trending news
+                window.localStorage.setItem("trending", "");
+                $(".loadTrendingNews").html('');
+                // loop to do this call four times 
+                for (var i = 0; i < 4; i++) {
+                    ///check if the element exists already in the browser localStorage to prevent duplication
+                    if (!window.localStorage.getItem(data.response.results[i].fields.headline)) {
+                         // create String off of the json file (the news)
+                        var dataString = JSON.stringify(data.response.results[i]);
+                         // add it to localStorage with its headline as a key 
+                        window.localStorage.setItem(data.response.results[i].fields.headline, dataString);
+                    }
+
+                    // add the headline to the array of Trending (keys are separated with %%)
+                    window.localStorage.setItem("trending", window.localStorage.getItem("trending") + "%%" + data.response.results[i].fields.headline)
+                    //getting date of the article
+                    // var dt2 = new Date(data.response.results[i].webPublicationDate);
+                    // // reunning the results of the article date with the date function lower down the page 
+                    // var res = diff_minutes(dt2);
+                    //  appending the info into the card styling and below that then I am using the i  to give for an i of which content is to be open when read more is clicked
+                    $(".loadTrendingNews").append(`
+                    <div class="col mb-4">
+                    <div class="card h-100">
+                        <h5 class="card-header">` +  data.response.results[i].sectionName +`</h5>
+                        <div class="card-body">
+                            <h5 class="card-title">`+ data.response.result.fields.headline +  `</h5>
+                            <img src="` + data.response.result.fields.thumbnail + `" class="card-img-top" alt="">
+                            <a href="#" class="btn btn-danger mt-2"><label for="modal_search_` + i + `">Read Full Story</label></a>
+                        </div>
+                    </div>
+                </div>
+                <input class="checker" type="checkbox" id="modal_search_` + i + `">
+                <div class="modal" style="max-width: 100vw; padding: 40px;">
+                    <div class="modal-body">
+                        <div class="modal-header">
+                            <h5 class="modal-title">` + data.response.result.fields.headline + `</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <label class="btn_close btn btn-outline-dark" for="modal_search_` + i + `"><i class="fa fa-times"></i></label>
+                            </button>
+                        </div>
+                        <img src="` + data.response.result.fields.thumbnail + `" alt="">
+                        <div class="modal-content">` + data.response.result.fields.body  + `</div>
+                    </div>
+                </div>
+                
+                                `);
+                }
+            }
+
+// OFFLINE
+//**
+//Step 1 - If API request fails (no network) fails initiate the html of trending stories, if not in local storage console log no results
+//Step 2 - If it wasn't searched before or stored show No data found
+//Step 3 - Break the string file up into an array of keys for example str= potato%%tomato%%carrot would then be x = [potato,tomato,carrot] after split
+//Step 4 - using first class from css file to output only 3 items per line
+//Step 5 - If i==3 articles (we already added 3 articles) the variable first user the value "first" for another line 
+//Step 6 - loop through keys like normal and get date from local storage 
+//** 
+}).fail(function (jqXHR, textStatus, errorThrown) {  
+    $(".loadTrendingNews").html('');
+    if (!window.localStorage.getItem("trending")) {
+        console.log('no results');
+        $(".loadTrendingNews").html('No data found against this search.');
+    } else {
+        resultss = window.localStorage.getItem("trending").split('%%');
+        for(var i=0; i<resultss.length;i++){
+            var result = JSON.parse(window.localStorage.getItem(resultss[i]));
+            if(result){
+                $(".loadTrendingNews").append(`
+                <div class="col mb-4">
+                                <div class="card h-100">
+                                    <h5 class="card-header">` + result.sectionName +`</h5>
+                                    <div class="card-body">
+                                        <h5 class="card-title">`+ result.fields.headline +  `</h5>
+                                        <img src="` + result.fields.thumbnail + `" class="card-img-top" alt="">
+                                        <a href="#" class="btn btn-danger mt-2"><label for="modal_search_` + i + `">Read Full Story</label></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <input class="checker" type="checkbox" id="modal_search_` + i + `">
+                            <div class="modal" style="max-width: 100vw; padding: 40px;">
+                                <div class="modal-body">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">` + result.fields.headline + `</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <label class="btn_close btn btn-outline-dark" for="modal_search_` + i + `"><i class="fa fa-times"></i></label>
+                                        </button>
+                                    </div>
+                                    <img src="` + result.fields.thumbnail + `" alt="">
+                                    <div class="modal-content">` + result.fields.body  + `</div>
+                                </div>
+                            </div>
+                
+                `);
+            }
+            
+        }
+    }
+
+});
+//**
+        // This is a refresh for content, a refresh function to refresh the page with new ajax calls. 
+        // what it is doing is forcing the page to reload and therefore load newest data.
+//**
+        setTimeout(getTrendingNews, 10800000);
+}
 
     //------------------------------------------------------- TOP STORIES SLIDER ONLINE & OFFLINE FUNCTION  ------------------------------------------
 // ONLINE FUNCTION
@@ -44,7 +175,7 @@ $(document).ready(function(){
                 <figcaption class="caption_trending_topics">
                 <h6 class="heading">` + data.response.results[i].fields.headline + `</h6>
                 <img src="` + data.response.results[i].fields.thumbnail + `" alt="">
-                <p ><label style ="font-size:1.1em;" class="pointer slider-label" for="modal_top_` + i + `"><span style="color:#b8a169;"> Full Story Here &raquo; </span> </label></p>
+                <footer><label class="btn btn-light mt-2"  for="modal_section_` + i + `">Read More</label></footer>
                 </figcaption>
                `);
                 $("#topStories").append(`
@@ -56,7 +187,8 @@ $(document).ready(function(){
                         <img src="` + data.response.results[i].fields.thumbnail + `" alt="">
                     <div class="modal-content">` + data.response.results[i].fields.body + `</div>
                     </div>
-            </div>`);
+            </div>
+            `);
             }
         }
 
@@ -145,14 +277,14 @@ function getUrlParam() {
                     window.localStorage.setItem(section, window.localStorage.getItem(section) + "%%" + data.response.results[i].fields.headline)
                     $("#Sections").append(`
                         <div class="card flex-md-row mb-4 box-shadow">
-                        <div class="card-body d-flex flex-column align-items-start">
+                        <div class="col-md-12 card-body d-flex flex-column align-items-start">
                         <div class="tag">` + data.response.results[i].sectionName + `</div>
                             <h3 class="mb-0">
                             <p>` + data.response.results[i].fields.headline + `</p>
                             </h3>
                             <div>` + data.response.results[i].fields.trailText + `</div>
                             <div>` + data.response.results[i].fields.main + `</div>
-                            <footer><label class="btn btn-danger"  for="modal_section_` + i + `">Read More</label></footer>
+                            <footer><label class="btn btn-danger mt-2"  for="modal_section_` + i + `">Read More</label></footer>
                         </div>
                         </div>
                     <input class="checker" type="checkbox" id="modal_section_` + i + `">
@@ -188,14 +320,14 @@ function getUrlParam() {
                     if(result){
                         $("#Sections").append(`
                                     <div class="card flex-md-row mb-4 box-shadow">
-                <div class="card-body d-flex flex-column align-items-start">
+                <div class="col-md-8 card-body d-flex flex-column align-items-start">
                 <div class="banner-title">` + result.sectionName + `</div>
                     <h3 class="mb-0">
                     <p>` + result.headline + `</p>
                     </h3>
                     <div>` + result.trailText + `</div>
                     <div>` + result.main + `</div>
-                    <footer><label class="btn btn-danger"  for="modal_section_` + i + `">Read More</label></footer>
+                    <footer><label class="btn btn-danger mt-2"  for="modal_section_` + i + `">Read More</label></footer>
                 </div>
                 </div>
             <input class="checker" type="checkbox" id="modal_section_` + i + `">
@@ -267,7 +399,7 @@ function searchNews() {
                                     <div class="card-body">
                                         <h5 class="card-title">`+ data.response.results[i].fields.headline +  `</h5>
                                         <img src="` + data.response.results[i].fields.thumbnail + `" class="card-img-top" alt="">
-                                        <a href="#" class="btn btn-danger"><label for="modal_search_` + i + `">Read Full Story</label></a>
+                                        <a href="#" class="btn btn-danger mt-2"><label for="modal_search_` + i + `">Read Full Story</label></a>
                                     </div>
                                 </div>
                             </div>
@@ -323,7 +455,7 @@ function searchNews() {
                                     <div class="card-body">
                                         <h5 class="card-title">`+ response.fields.headline +  `</h5>
                                         <img src="` + response.fields.thumbnail + `" class="card-img-top" alt="">
-                                        <a href="#" class="btn btn-danger"><label for="modal_search_` + o + `">Read Full Story</label></a>
+                                        <a href="#" class="btn btn-danger mt-2"><label for="modal_search_` + o + `">Read Full Story</label></a>
                                     </div>
                                 </div>
                             </div>
